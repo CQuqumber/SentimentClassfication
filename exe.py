eabb0e65a94dfb3e5f32319770cee653ef5070d6
Kueipo <cququmber@gmail.com>
2017-02-17 12:50:51 +0800

    bug fixed

--- exe.py
+++ exe.py
@@ -1,10 +1,6 @@
 from sentiment import *
 from preview_label import *
 
-mlp = SentimentNetwork(reviews[:-1000],labels[:-1000], learning_rate=0.001)
-
-# evaluate our model before training (just to show how horrible it is)
-mlp.test(reviews[-1000:],labels[-1000:])
-
-# train the network
+mlp = SentimentNetwork(reviews[:-1000],labels[:-1000], learning_rate=0.1)
 mlp.train(reviews[:-1000],labels[:-1000])
+mlp.test(reviews[-1000:],labels[-1000:])
--- sentiment.py
+++ sentiment.py
@@ -54,14 +54,15 @@ class SentimentNetwork:
 		self.learning_rate = learning_rate
 
 		self.layer_0 = np.zeros((1, input_nodes))
+		self.layer_1 = np.zeros((1, hidden_nodes))
 
 	def update_input_layer(self, review):
 		# clear out previous state, reset the layer to be all 0s
 		self.layer_0 *= 0
 		for word in review.split(" "):
-			if(word in self.word2index.key()):
+			#if(word in self.word2index.key()):	# Redundant
 				#self.layer_0[0][self.word2index[word]] += 1
-				self.layer_0[self.word2index[word]] = 1	#  Noise reduction
+			self.layer_0[0][self.word2index[word]] = 1	#  Noise reduction
 
 	def get_target_for_label(self, label):
 		if(label == 'POSITIVE'):
@@ -77,7 +78,16 @@ class SentimentNetwork:
 		return output * (1 - output)
 
 
-	def train(self, training_reviews, training_labels):
+	def train(self, training_reviews_raw, training_labels):
+		
+		training_reviews = list()
+		for review in training_reviews_raw:
+			indices = set()
+			for word in review.split(" "):
+				if(word in self.word2index.keys()):
+					indices.add(self.word2index[word])
+			training_reviews.append(list(indices))
+
 		#insert debugging assertions
 		assert(len(training_reviews) == len(training_labels))
 
@@ -93,37 +103,38 @@ class SentimentNetwork:
 			# Forward pass
 
 			# Input Layer
-			self.update_input_layer(review)
+			#self.update_input_layer(review)
 
 			# Hidden layer
-			layer_1 = self.layer_0.dot(self.weights_0_1)
+			#layer_1 = self.layer_0.dot(self.weights_0_1)
+			self.layer_1 *= 0
+			for index in review:
+				self.layer_1 += self.weights_1_2[index]
 
 			# Output layer
 			layer_2 = self.sigmoid(layer_1.dot(self.weights_1_2))
 
 			### Backward pass
-
 			## Output error
 			layer_2_error = layer_2 - self.get_target_for_label(label)
 			#  O/p error difference between desire and actual
-
 			layer_2_delta = layer_2_error * self.sigmoid_output_2_derivative(layer_2)
 
 			## Baclpropagated error
-
 			layer_1_error = layer_2_delta.dot(self.weights_1_2.T)
 			# error propagated to the hidden layer
-
 			layer_1_delta = layer_1_error
 			# hidden layer gradient - no nonlinearity so it's the same as the error
 
 			## Update the weights
-
 			self.weights_1_2 -= layer_1.T.dot(layer_2_delta) * self.learning_rate
 			# update hidden - to - output weight with gradient descent step
 
-			self.weights_0_1 -= self.layer_0.T.dot(layer_1_delta) * self.learning_rate
+			#self.weights_0_1 -= self.layer_0.T.dot(layer_1_delta) * self.learning_rate
 			# update the input 2 hidden weights with gradient descent step
+
+			for index in review:
+				self.weights_0_1[index] -= layer_1_delta[0] * self.learning_rate
 
 			if(np.abs(layer_2_error) < 0.5):
 				correct_so_far += 1
@@ -159,36 +170,22 @@ class SentimentNetwork:
 	def run(self, review):
 
 		# Input Layer
-		self.update_input_layer(review.lower())
+		#self.update_input_layer(review.lower())
 
 		# Hidden Layer
-		layer_1 = self.layer_0.dot(self.weights_0_1)
+		self.layer_1 *= 0
+		unique_indices = set()
+		for word in review.lower().split(" "):
+			if word in self.word2index.key():
+				unique_indices.add(self.word2index[word])
+		for index in unique_indices:
+			self.layer_1 += self.weights_0_1[index]
 
 		# Output layer
-		layer_2 = self.sigmoid(layer_1.dot(self.weights_1_2))
+		layer_2 = self.sigmoid(self.layer_1.dot(self.weights_1_2))
 
 		if(layer_2[0] > 0.5):
 			return 'POSITIVE'
 		else:
 			return 'NEGATIVE'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
