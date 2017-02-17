a6ceedff58db9f7e825473ad67793c1512d0b32c
Kueipo <cququmber@gmail.com>
2017-02-17 13:32:15 +0800

    modofied exe

--- exe.py
+++ exe.py
@@ -2,5 +2,7 @@ from sentiment import *
 from preview_label import *
 
 mlp = SentimentNetwork(reviews[:-1000],labels[:-1000], learning_rate=0.1)
+
 mlp.train(reviews[:-1000],labels[:-1000])
+
 mlp.test(reviews[-1000:],labels[-1000:])
